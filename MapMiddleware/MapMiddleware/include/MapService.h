aa997da94a59036341d1f0ad892a0a551d6fd9cb
deverwh <deverwh@outlook.com>
2020-01-01 20:00:29 +0800

    添加地图中间件项目

---
+++ MapMiddleware/MapMiddleware/include/MapService.h
@@ -0,0 +1,22 @@
+/*!
+ * \文件 MapService.h
+ * \日期 2020/01/01 19:57
+ *
+ * \作者 wanghao
+ * 联系: deverwh@outlook.com
+ *
+ * \简介 地图实例创建服务类
+ *		单例
+ *
+ * \更改 2020.1.1 新建
+*/
+
+#pragma once
+
+class MapHandle;
+
+class MapService
+{
+public:
+
+};
