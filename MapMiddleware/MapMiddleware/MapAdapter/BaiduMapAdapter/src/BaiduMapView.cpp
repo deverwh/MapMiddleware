8b9031fd6cfa018c738eee9db9511c44edd34d38
Deverwh <303792351@qq.com>
2020-03-20 23:02:03 +0800

    使用run

--- MapMiddleware/MapMiddleware/MapAdapter/BaiduMapAdapter/src/BaiduMapView.cpp
+++ MapMiddleware/MapMiddleware/MapAdapter/BaiduMapAdapter/src/BaiduMapView.cpp
@@ -27,12 +27,20 @@ BaiduMapView::~BaiduMapView()
 
 void BaiduMapView::addPoint(const QString &poingJson)
 {
-	emit sendAddPointMessage(poingJson);
+	QString cmd = QString("addPoint(%1)")
+		.arg(poingJson);
+	//把要调用的JS命令当做QString传递给网页
+	//对应baidumap.html 第77行
+	//这就实现了QT通过C++调用JS函数，QT向网页传递数据
+	this->page()->runJavaScript(cmd);
+	//emit sendAddPointMessage(poingJson);
 }
 
 void BaiduMapView::removeMapItem(const QString &id)
 {
-	emit removeOverlay(id);
+	QString cmd = QString("removeOverlay(\"%1\")")
+		.arg(id);
+	this->page()->runJavaScript(cmd);
 }
 
 void BaiduMapView::mouseMoved(qreal longitude, qreal latitude)
